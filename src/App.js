5beaf82a68b0ac21186331e4be8f6e5100380563
wizArD-1910 <aman.d@ahduni.edu.in>
2019-11-06 21:13:15 +0530

    AWS integrated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
-import "./App.css";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
+import "./App.css";
 import Routes from "./Routes";
 
 function App(props) {
@@ -15,6 +16,16 @@ function App(props) {
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
